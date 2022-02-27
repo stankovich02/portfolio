e1e0d469562a1db4824c496df175fe709d43982d
Marko Stankovic <marefaca2002@gmail.com>
2022-02-27 11:47:49 +0100

    update

--- contact.php
+++ contact.php
@@ -1,22 +1,22 @@
 <?php
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
-    $visitore_email = $_POST['mail'];
+    $visitor_email = $_POST['mail'];
     $service = $_POST['service'];
     $message = $_POST['message'];
 
     $email_from = 'stankovichportfolio@gmail.com';
     $email_subject = "New Form Submission";
-    $email_body = "User Name: $fistname $lastname.\n".
+    $email_body = "User Name: $firstname $lastname.\n".
                     "User Email: $visitor_email.\n".
-                    "Service: $service.\n"
+                    "Service: $service.\n".
                     "User message: $message.\n";
 
     $to = "marefaca2002@gmail.com";
 
     $headers = "From: $email_from \r\n";
 
-    $headers .= "Reply-To: $visitor-email \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
     mail($to,$email_subject,$email_body,$headers);
 
